d0359c8aa1f74053e51d4bb5bd71e3b3fcc99062
Mahinth Joe Christensen <contact@mahinthjoe.com>
2021-04-29 15:44:53 +0530

    Removed cask from brew command'

--- brew-cask.sh
+++ brew-cask.sh
@@ -11,43 +11,43 @@ brew install caskroom/cask/brew-cask
 # brew tap caskroom/versions
 
 # daily
-brew cask install spectacle
-brew cask install dropbox
-brew cask install gyazo
-brew cask install 1password
-brew cask install rescuetime
-# brew cask install flux
+brew install spectacle
+brew install dropbox
+brew install gyazo
+brew install 1password
+brew install rescuetime
+# brew  install flux
 
 # dev
-brew cask install iterm2
-brew cask install sublime-text
-brew cask install imagealpha
-brew cask install imageoptim
+brew install iterm2
+brew install sublime-text
+brew install imagealpha
+brew install imageoptim
 
 # fun
-brew cask install limechat
-# brew cask install miro-video-converter
-# brew cask install horndis               # usb tethering
+brew  install limechat
+# brew  install miro-video-converter
+# brew  install horndis               # usb tethering
 
 # browsers
-# brew cask install google-chrome-canary
-# brew cask install firefoxnightly
-# brew cask install webkit-nightly
-# brew cask install chromium
-# brew cask install torbrowser
+# brew  install google-chrome-canary
+# brew  install firefoxnightly
+# brew  install webkit-nightly
+# brew  install chromium
+# brew  install torbrowser
 
 # less often
-brew cask install disk-inventory-x
-brew cask install screenflow
-brew cask install vlc
-brew cask install gpgtools
-brew cask install licecap
-# brew cask install utorrent
+brew install disk-inventory-x
+brew install screenflow
+brew install vlc
+brew install gpgtools
+brew install licecap
+# brew  install utorrent
 
-brew cask install spotify
+#brew install spotify
 
-brew tap caskroom/fonts 
-brew cask install font-fira-code
+brew  caskroom/fonts 
+brew  install font-fira-code
 
 
 # Not on cask but I want regardless.
